1fc253d388e19bbbbf9e281dd63f5b0585db10e6
Paul Huber <147988390+pahu257@users.noreply.github.com>
2023-12-18 00:22:45 +0100

    Create Application.java

---
+++ Application.java
@@ -0,0 +1,13 @@
+package web_anwendung.benutzeroberflaeche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenutzeroberflaecheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenutzeroberflaecheApplication.class, args);
+	}
+
+}
